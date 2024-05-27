9d0a46694ac24fba0d730c0984ac975826b30d14
kerlonfernandes <kerlon1221@gmail.com>
2024-05-27 12:43:19 -0300

    email sendf

--- admin/ajax/teste.php
+++ admin/ajax/teste.php
@@ -1,20 +1,23 @@
 <?php
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require_once '../../vendor/autoload.php';
 require "../../_app/Config.inc.php";
-require "../../classes/Email.inc.php";
+require "../../classes/Email.inc.php"; // Substitua com o caminho correto para o arquivo Email.inc.php
 
 $email = new Email();
-
 
 $subject = 'Olá teste';
 $body = '<p>Teste</p>';
 $recipient_name = 'KErlon';
 $recipient_email = 'kerlon1221@gmail.com';
 
-
 $email->add($subject, $body, $recipient_name, $recipient_email);
 
-// // Anexos (opcional)
+// Anexos (opcional) - Descomente esta seção se precisar de anexos
 // $filePath = '/caminho/para/o/arquivo.pdf';
 // $fileName = 'arquivo.pdf';
 // $email->attach($filePath, $fileName);
@@ -25,4 +28,5 @@ if ($email->send()) {
 } else {
     echo 'Houve um erro ao enviar o email: ' . $email->error()->getMessage();
 }
+
 ?>
